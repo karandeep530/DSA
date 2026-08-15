bbf75eb5aad51badfc04fa205fb58233f862a84e
Karandeep Singh <anureet5454@gmail.com>
2026-08-15 22:15:02 +0530

    leapyear

---
+++ leapyear.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+using namespace std;
+int main(){
+    int year;
+    cin>>year;
+    if(year%400==0){
+        cout<<"leap year"<<endl;}
+        else
+        cout<<"not a leap year"<<endl;
+    
+}
